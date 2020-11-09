9d27b0a45307b9bc7e9beba73985305ec53e6422
ruben cohen <ruben.cohen75@outlook.com>
2020-11-09 10:13:58 +0100

    comit initial du projet

---
+++ appli/vues/vSupprimerUt.php
@@ -0,0 +1,14 @@
+<!--Formulaire de suppression à partir de l'identifiant -->
+
+<div class="container">
+
+<form action="" method=post>
+<fieldset>
+<legend>Entrez la référence de l'id du client à supprimer </legend>
+<label> Id:</label> <input type="text" name="id" size="10" />
+</fieldset>
+<button type="submit" class="btn btn-primary">Supprimer</button>
+<button type="reset" class="btn">Annuler</button>
+</form>
+
+</div>
